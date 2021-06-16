b6efbbd27ebe53dcdcd388039280d774d4073128
jesushledon@gmail.com <jesushledon@gmail.com>
2021-06-16 00:52:42 -0500

    Ok.. I moved the model to main, i dont know hot to get the module by models with out having a circular dependency, now exits two creator scripts, as flask scripts

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import request, make_response, redirect, render_template
 from app import create_app
+# Database filler from git_hub
+from scripts.seed import Seed
+# custom commands
+import click
 
 
 app = create_app()
@@ -26,3 +30,43 @@ def hellow():
 @app.errorhandler(404)
 def not_foud(error):
     return render_template('404.html', error=error)
+
+
+# CUSTOM COMMANDS
+@app.cli.command("flask-script")
+@click.argument("num_users")
+def flask_script(num_users=150):
+    seed = Seed()
+    users = seed.get_users(num_users=int(num_users))
+    for user in users:
+        try:
+            app.db.session.add(
+                Coder(
+                    id=user.get('id'),
+                    username=user.get('login', ''),
+                    avatar=user.get('avatar_url', ''),
+                    type=user.get('type', ''),
+                    url=user.get('url', '')
+                )
+            )
+            app.db.session.commit()
+        except Exception as error:
+            ## Integrity safe for ids
+            pass
+
+
+
+@app.cli.command("init")
+def app_init():
+    app.db.create_all()
+
+
+class Coder(app.db.Model):
+    id = app.db.Column(app.db.Integer, primary_key=True)
+    username = app.db.Column(app.db.String(25), unique=True, nullable=False)
+    avatar = app.db.Column(app.db.String(120), nullable=False)
+    url = app.db.Column(app.db.String(120), unique=True, nullable=False)
+    type = app.db.Column(app.db.String(5), nullable=False)
+
+    def __repr__(self):
+        return "{self.username} {self.user}"
