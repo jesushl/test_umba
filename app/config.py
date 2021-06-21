036cce4316b0ed4627a971337c5e70c0e59e761e
jesushledon@gmail.com <jesushledon@gmail.com>
2021-06-20 23:40:28 -0500

    Changing config to read  heroku database

--- app/config.py
+++ app/config.py
@@ -1,5 +1,10 @@
+import os
 class Config:
     SECRET_KEY = "UBUNTU"
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///main/database/git_hub_test.db"
+    SQLALCHEMY_DATABASE_URI = database = os.environ.get(
+        'DATABASE_URL',
+        "sqlite:///main/database/git_hub_test.db"
+    )
+    if os.environ.get()
     SQLALCHEMY_TRACK_MODIFICATIONS=True
