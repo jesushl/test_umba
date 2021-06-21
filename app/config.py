4c1e736a2e6600391f2ff5d9f5a6738d0c5ada96
jesushledon@gmail.com <jesushledon@gmail.com>
2021-06-20 23:44:38 -0500

    removing a bad configuration from sqlalchemy url

--- app/config.py
+++ app/config.py
@@ -3,8 +3,9 @@ class Config:
     SECRET_KEY = "UBUNTU"
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = database = os.environ.get(
-        'DATABASE_URL',
-        "sqlite:///main/database/git_hub_test.db"
+        'DATABASE_URL'
     )
+    if not SQLALCHEMY_DATABASE_URI:
+        SQLALCHEMY_DATABASE_URI = "sqlite:///main/database/git_hub_test.db"
     if os.environ.get()
     SQLALCHEMY_TRACK_MODIFICATIONS=True
